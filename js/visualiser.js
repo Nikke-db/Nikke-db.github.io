71e3fc95659c7d436dd716bfb9d0128697344aea
Koshirei <axelaxelc@aol.fr>
2022-09-23 04:07:13 +0200

    fix c232 fb l2d

--- js/visualiser.js
+++ js/visualiser.js
@@ -123,7 +123,7 @@ const changeSpine = (id) => {
 let current_l2d = "fb"
 // let current_l2d = "aim"
 // let current_l2d = "cover"
-changeSpine("c010")
+changeSpine("c232")
 
 let move = false
 let oldx = "";
