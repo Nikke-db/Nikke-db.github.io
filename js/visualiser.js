8e90117219b3c91e4fbf383fa7be79c15c2664dd
Koshirei <axelaxelc@aol.fr>
2022-10-24 14:36:05 +0200

    add Pepper l2d

--- js/visualiser.js
+++ js/visualiser.js
@@ -126,10 +126,10 @@ const changeSpine = (id) => {
 }
 
 
-let current_l2d = "fb"
+// let current_l2d = "fb"
 // let current_l2d = "aim"
-// let current_l2d = "cover"
-changeSpine("c212")
+let current_l2d = "cover"
+changeSpine("c131")
 
 let move = false
 let oldx = "";
